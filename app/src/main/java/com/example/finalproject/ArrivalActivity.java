aa2dee23fbe3cfa534e6cd44ebba2cd6a94aaefb
Mizer <jack310216@gmail.com>
2022-06-10 05:54:49 +0800

    外送功能完善

--- app/src/main/java/com/example/finalproject/ArrivalActivity.java
+++ app/src/main/java/com/example/finalproject/ArrivalActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ArrivalActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class ArrivalActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_arrival);
   }
+
+  public void goRiderend (View view){
+    Intent intent = new Intent (this, RiderendActivity.class);
+    startActivity(intent);
+  }
+
 }
