c06178c901c5de7836892ad272a841f8692d54cf
chengyu_mac <yces4199600@gmail.com>
2022-06-10 03:19:13 +0800

    又回到最初的原點

---
+++ app/src/main/java/com/example/finalproject/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_profile);
+  }
+  public void goBackMain(View view){
+    finish();
+  }
+}
+//goBackMain
